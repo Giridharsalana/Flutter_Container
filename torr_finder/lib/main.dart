3bf6db5f1fca396e105e38733fafb7e50afba37f
Giridhar Salana <giridharsalana@gmail.com>
2022-03-05 19:17:38 +0000

    Updates

--- torr_finder/lib/main.dart
+++ torr_finder/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,33 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Torr Finder',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color(0xFF26166b),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Torr Finder'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +34,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void homeset() {
+    setState(() 
+      {
+      }
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(widget.title,style: TextStyle(fontFamily: 'Pacifico')),
+        elevation: 0,
+        backgroundColor: Color(0xFF26166b),
+        centerTitle: true,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                homeset();
+              },
+            );
+          }
         ),
+        actions: <Widget>[
+          Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.account_circle),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)), //this right here
+                        child: Container(
+                          height: 350,
+                          width: 280,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Developer',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: 'Pacifico',
+                                    fontSize: 25.0,
+                                  ),
+                                ),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: Image.asset(
+                                    "assets/images/Giri.jpg",
+                                    width: 180,
+                                    height: 200,
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                                Text(
+                                  'Giridhar Salana',
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                    fontFamily: 'Pacifico',
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center, 
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(FontAwesomeIcons.facebook, color: Colors.red),
+                                      onPressed: () async {
+                                        const fburl = 'https://www.facebook.com/GiridharSalana';
+                                        if (await canLaunch(fburl)) {
+                                          await launch(fburl);
+                                        } else {
+                                          throw 'Could not launch $fburl';
+                                        }
+                                      }),
+                                    SizedBox(width: 20),
+                                    IconButton(
+                                      icon: Icon(FontAwesomeIcons.linkedin, color: Colors.red),
+                                      onPressed: () async {
+                                        const liurl = 'https://www.linkedin.com/in/Giridhar-Salana';
+                                        if (await canLaunch(liurl)) {
+                                          await launch(liurl);
+                                        } else {
+                                          throw 'Could not launch $liurl';
+                                        }
+                                      }),
+                                    SizedBox(width: 20),
+                                    IconButton(
+                                      icon: Icon(FontAwesomeIcons.github, color: Colors.red),
+                                      onPressed: () async {
+                                        const giurl = 'https://github.com/Giridharsalana';
+                                        if (await canLaunch(giurl)) {
+                                          await launch(giurl);
+                                        } else {
+                                          throw 'Could not launch $giurl';
+                                        }
+                                      }),
+                                    SizedBox(width: 20),
+                                    IconButton(
+                                      icon: Icon(FontAwesomeIcons.twitter, color: Colors.red),
+                                      onPressed: () async {
+                                        const tiurl = 'https://www.twitter.com/GiridharSalana3';
+                                        if (await canLaunch(tiurl)) {
+                                          await launch(tiurl);
+                                        } else {
+                                          throw 'Could not launch $tiurl';
+                                        }
+                                      }
+                                    ),
+                                  ]
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  );
+                },
+              );
+            }
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
